1a614dc3257d7182a7bddf433c8cb306c9c1b74e
Devin Frost <devin.frost4311@gmail.com>
2024-06-10 11:20:39 -0400

    Engine is no longer forward declared. This needs fixed so that Props can talk to each other again Added support for glm The old flag system was depricated in favor of using Prop*'s for both easy management and ease of use. setPropIndex renamed to setID Implemented Prop.draw. It simply iterates through components and calls their draw functions. Added methods to both add and remove props

--- prop.h
+++ prop.h
@@ -2,8 +2,15 @@
 #define PROP_H
 
 #pragma once
-#include "vec2.h"
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+#include <glm/gtx/quaternion.hpp>
 #include "Engine.h"
+#include <vector>
+#include "Geometry.h"
+#include "Component.h"
 
 // Throws 'name followed by "::" must be a class or namespace name' within kernel.cu when uncommented
 //#include "kernel.cu"
@@ -11,125 +18,168 @@
 #define PROP_ATTRIBUTE_COUNT 9
 
 
-class Engine;
 
 class Prop
 {
 public:
-	
-	
-	static constexpr short PROP_DEBUG = 1;
-	
 	Prop(int prop_idx);
 	Prop();
 	~Prop();
-	void setPropIndex(int idx);
-
-
 	void draw();
+	void setPosition(const glm::vec3& position);
+	glm::vec3 getPosition() const;
+	void setRotation(const glm::quat& rotation);
+	glm::quat getRotation() const;
+	void onTick();
 
-	// Gets a prop's position in screen space
-	vec2 getPosition();
+	void setID(int ID);
+	int getID() const;
 
-	// Sets a prop's position in screen space
-	void setPosition(vec2 new_position);
-
-
-
-	// Gets a prop's velocity (in pixels / second)
-	vec2 getVelocity();
-
-	// Sets a prop's velocity (in pixels / second)
-	void setVelocity(vec2 new_velocity);
+	bool addComponent(Component* added_component);
+	void removeComponent(Component* removed_component);
 
 
-
-	// Gets a prop's acceleration (in pixels / second^2)
-	vec2 getAcceleration();
-
-	// Sets a prop's acceleration (in pixels / second^2)
-	void setAcceleration(vec2 new_velocity);
-
-
-
-	// Gets a prop's rotation (in radians)
-	float getRotation();
-
-	// Sets a prop's rotation (in radians)
-	void setRotation(float new_rotation);
-
-
-
-	// Gets a prop's rotational velocity (radians / second)
-	float getRotationalVelocity();
-
-	// Sets a prop's rotational velocity (radians / second)
-	void setRotationalVelocity(float new_rotational_velocity);
-
-
-
-	// Gets a prop's rotational velocity (radians / second^2)
-	float getRotationalAcceleration();
-
-	// Sets a prop's rotational velocity (radians / second^2)
-	void setRotationalAcceleration(float new_rotational_velocity);
+	//int find_slot();
+	//void cleanup(); // Optimizes memory layout with all occupied component slots on the right and empty ones on the left. This allows lots of optimizations to be made. This will be handled at a later date.
 
 
 private:
-	int prop_ID = 0;
-	short flags;
+	std::vector<Component*> components;
+	int prop_ID;
+	glm::mat4 transform;
+	glm::vec3 velocity;
+	glm::vec3 acceleration;
 
+	glm::vec3 rotational_velocity;
+	glm::vec3 rotational_acceleration;
+	int partition_index = 0;
 };
+Prop::Prop():
 
-Prop::Prop(int prop_idx)
-{
-	flags = PROP_DEBUG;
-	prop_ID = prop_idx;
-}
+	// Identity matrix. Sets position and rotation to 0, but scale to 1.0 along all axes
+	transform(1.0f),
+	// No velocity
+	velocity(0.0f),
+	// No acceleration
+	acceleration(0.0f),
+	// No rotational velocity
+	rotational_velocity(0.0f),
+	// No rotational acceleration
+	rotational_acceleration(0.0f),
 
-Prop::Prop() { flags = PROP_DEBUG; }
+	prop_ID(0) // Replace with Engine::find_slot
+
+{}
+
+Prop::Prop(int idx) :
+
+	// Identity matrix. Sets position and rotation to 0, but scale to 1.0 along all axes
+	transform(1.0f),
+	// No velocity
+	velocity(0.0f),
+	// No acceleration
+	acceleration(0.0f),
+	// No rotational velocity
+	rotational_velocity(0.0f),
+	// No rotational acceleration
+	rotational_acceleration(0.0f),
+	// Also set the prop_ID
+	prop_ID(idx)
+	// Set the geometry to the appropriate index
+
+{};
 
 Prop::~Prop() = default;
-void Prop::setPropIndex(int idx) { prop_ID = idx; }
 
 
-void Prop::draw()
-{
-	//SDL_RenderDrawRect()
+void Prop::setPosition(const glm::vec3& position) {
+	transform[3] = glm::vec4(position, 1.0f);
 }
 
-vec2 Prop::getPosition() { return Engine::getInstance().getPropPosition(prop_ID); }
+glm::vec3 Prop::getPosition() const
+{
+	return glm::vec3(transform[3]);
+}
 
-void Prop::setPosition(vec2 new_position) { return Engine::getInstance().setPropPosition(prop_ID,new_position); }
+void Prop::setRotation(const glm::quat& rotation) {
+	// Convert the quaternion to a rotation matrix
+	//glm::mat3 rotationMatrix = glm::toMat3(rotation);
+	glm::mat3 rotationMatrix = glm::mat3_cast(rotation);
+	// Copy the rotation matrix into the upper-left 3x3 part of the transformation matrix
+	memcpy(&transform[0][0], &rotationMatrix[0][0], sizeof(float) * 3); // Copy first column (X-axis)
+	memcpy(&transform[1][0], &rotationMatrix[1][0], sizeof(float) * 3); // Copy second column (Y-axis)
+	memcpy(&transform[2][0], &rotationMatrix[2][0], sizeof(float) * 3); // Copy third column (Z-axis)
+}
 
+glm::quat Prop::getRotation() const {
+	return glm::quat_cast(glm::mat3(transform));
+}
+void Prop::onTick()
+{
+	for (size_t i = 0; i < components.size(); i++)
+	{
+		components[i]->onTick();
+	}
+}
+void Prop::draw()
+{
+	for (size_t i = 0; i < partition_index; i++)
+	{
+		if (components[i])
+		{
+			components[i]->draw();
+		}
+	}
+}
 
-vec2 Prop::getVelocity() { return Engine::getInstance().getPropVelocity(prop_ID); }
+void Prop::setID(int ID)
+{
+	prop_ID = ID;
+}
+int Prop::getID() const
+{
+	return prop_ID;
+}
+bool Prop::addComponent(Component* added_component)
+{
+	if (partition_index < components.size())
+	{
+		components[partition_index] = added_component;
+		partition_index++;
+	}
+	else
+	{
+		components.push_back(added_component);
+		partition_index++;
+	}
+	return 0;
+}
+void Prop::removeComponent(Component* removed_component)
+{
+	if (removed_component==nullptr) { return; } // Check for nullptr first
+	// Get the prop's index. 
+	int deleted_index = -1;
+	for (size_t i = 0; i < partition_index; i++)
+	{
+		if (components[i] == removed_component)
+		{
+			deleted_index = i; break;
+		}
+	}
+	if (deleted_index == -1) { return; } // If the requested component to remove is invalid, exit. Need to make this check in Engine, too
 
-void Prop::setVelocity(vec2 new_velocity) { Engine::getInstance().setPropVelocity(prop_ID, new_velocity); }
+	// Delete the prop
+	delete removed_component;
 
+	// Since this prop is now invalid, move the last valid prop here
+	components[deleted_index] = components[partition_index];
 
+	// Since the last prop is pointed to twice in the array, set the original index to nullptr
+	components[partition_index] = nullptr;
 
+	// Since the original pointer is invalidated, advance the partition index backwards
+	partition_index--;
 
-vec2 Prop::getAcceleration() { return Engine::getInstance().getPropAcceleration(prop_ID); }
-
-
-void Prop::setAcceleration(vec2 new_velocity) { Engine::getInstance().setPropAcceleration(prop_ID, new_velocity); }
-
-
-float Prop::getRotation() { return Engine::getInstance().getPropRotation(prop_ID); }
-
-
-void Prop::setRotation(float new_rotation) { Engine::getInstance().setPropRotation(prop_ID, new_rotation); }
-
-
-float Prop::getRotationalVelocity() { return Engine::getInstance().getPropRotationalVelocity(prop_ID); }
-
-
-void Prop::setRotationalVelocity(float new_rotational_velocity) { Engine::getInstance().setPropRotationalVelocity(prop_ID, new_rotational_velocity); }
-
-float Prop::getRotationalAcceleration() { return Engine::getInstance().getPropRotationalAcceleration(prop_ID); }
-
-
-void Prop::setRotationalAcceleration(float new_rotational_velocity) { Engine::getInstance().setPropRotationalAcceleration(prop_ID, new_rotational_velocity); }
+}
 
 #endif // PROP_H
