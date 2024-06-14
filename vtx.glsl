1b0d33fb789ae863bb3a836ef652af13a3ebee13
Devin Frost <devin.frost4311@gmail.com>
2024-06-14 16:48:36 -0400

    Updated vertex shader to draw relative to Prop and view

--- vtx.glsl
+++ vtx.glsl
@@ -1,8 +1,12 @@
 #version 330 core
 
-layout(location = 0) in vec3 aPos; // Vertex position attribute
+layout(location = 0) in vec3 aPos;
 
-void main()
-{
-    gl_Position = vec4(aPos, 1.0); // Output the vertex position
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+uniform mat4 parentTransform;
+
+void main() {
+    gl_Position = projection * view * parentTransform * model * vec4(aPos, 1.0);
 }
