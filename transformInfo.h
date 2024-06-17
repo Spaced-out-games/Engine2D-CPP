880b8a6fd0d5348c050e67d0ec2f3a8bace77e34
Devin Frost <devin.frost4311@gmail.com>
2024-06-17 00:20:59 -0400

    Added documentation

--- transformInfo.h
+++ transformInfo.h
@@ -2,9 +2,20 @@
 #ifndef TRANSFORMINFO_H
 #define TRANSFORMINFO_H
 #pragma once
+
+
+
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
 #include <glm/gtx/quaternion.hpp>
+
+/// <summary>
+/// There's one of these classes for every Prop in the scene (then some for allocated spaces)
+/// Moving the pointer of a Prop via insertions and deletions will move these to the appropriate index so each is properly paired up
+/// If you want to add more contiguous data to the class, that's your perogative. The main goal of this is to compartmentalize
+/// </summary>
+
+
 
 class transformInfo
 {
@@ -35,20 +46,29 @@ transformInfo::transformInfo():
 	rotational_acceleration(0.0f)
 {}
 
+// Helper funtion for getting the position from the transform
 glm::vec3 transformInfo::getPosition() { return glm::vec3(transform[3]); }
 
+// Helper function for setting the position from the transform
 void transformInfo::setPosition(glm::vec3 new_position) { transform[3] = glm::vec4(new_position, 1.0f); }
 
+// Helper function for setting the rotation of the transform
 void transformInfo::setRotation(const glm::quat& rotation)
 {
 	// Convert the quaternion to a rotation matrix
 	glm::mat3 rotationMatrix = glm::mat3_cast(rotation);
-	// Copy the rotation matrix into the upper-left 3x3 part of the transformation matrix
+
+	// Copy the rotation matrix into the upper-left 3x3 part of the transformation matrix. 
+	// This is the most efficient means of doing it. I think it's safe enough
 	memcpy(&transform[0][0], &rotationMatrix[0][0], sizeof(float) * 3); // Copy first column (X-axis)
 	memcpy(&transform[1][0], &rotationMatrix[1][0], sizeof(float) * 3); // Copy second column (Y-axis)
 	memcpy(&transform[2][0], &rotationMatrix[2][0], sizeof(float) * 3); // Copy third column (Z-axis)
+
 }
+
+// Helper function for getting the rotation from the transform
 glm::quat transformInfo::getRotation() const {
 	return glm::quat_cast(glm::mat3(transform));
 }
+
 #endif
