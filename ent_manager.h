69ca9b144a8cd921b407a70795ffc555d06cde9a
Devin Frost <devin.frost4311@gmail.com>
2024-07-01 18:06:20 -0400

    Moved the ent_manager to its own file

---
+++ ent_manager.h
@@ -0,0 +1,51 @@
+#ifndef ENT_MANAGER_H
+#define ENT_MANAGER_H
+
+#include <any>
+#include <vector>
+#include <typeindex>
+#include <stdexcept>
+#include <memory>
+#include "ent_array.h"
+
+class ent_manager
+{
+public:
+    template<typename T>
+    void add_array()
+    {
+        // Create and store a new ent_array<T> instance
+        arrays_.emplace_back(ent_array<T>());
+    }
+
+    template<typename T>
+    ent_array<T>& get_array()
+    {
+        for (auto& arr : arrays_)
+        {
+            if (auto* typed_arr = std::any_cast<ent_array<T>>(&arr))
+            {
+                return *typed_arr;
+            }
+        }
+        throw std::out_of_range("No ent_array found for the specified type");
+    }
+
+    template<typename T>
+    const ent_array<T>& get_array() const
+    {
+        for (const auto& arr : arrays_)
+        {
+            if (const auto* typed_arr = std::any_cast<const ent_array<T>>(&arr))
+            {
+                return *typed_arr;
+            }
+        }
+        throw std::out_of_range("No ent_array found for the specified type");
+    }
+
+private:
+    std::vector<std::any> arrays_;
+};
+
+#endif // ENT_ARRAY_MANAGER_H
