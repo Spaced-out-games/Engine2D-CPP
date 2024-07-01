e6e3d4d6a81031ea06954a8d2a37964b5016f832
Devin Frost <devin.frost4311@gmail.com>
2024-07-01 18:06:05 -0400

    Moved the ent_handle to its own file

---
+++ ent_handle.h
@@ -0,0 +1,15 @@
+#ifndef ENT_HANDLE_H
+#define ENT_HANDLE_H
+#include "bitset_512.h"
+
+
+#define type_t bitset_size_t // for now until I have a better way to discern types
+typedef struct ent_handle
+{
+	ent_handle(bitset_size_t entity_index) { type_ID = 0; block_ID = 0; ent_index = entity_index; }
+	type_t type_ID;
+	bitset_size_t block_ID;
+	bitset_size_t ent_index;
+};
+
+#endif
