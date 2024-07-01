9b0f09dde3c5da710768eefaed109406adbe182b
Devin Frost <devin.frost4311@gmail.com>
2024-07-01 18:06:29 -0400

    Test code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
-#include "shared_types.h"
+#include "ent_manager.h"
+#include <iostream>
 
 int main()
 {
-	//ent_chunk<int> integers;
-	//const bitset_512& the_bitset = integers.getStates();
-	//std::cout << the_bitset.get(0);
-	//integers.insert(42);
-	//std::cout << the_bitset.get(0);
+    ent_manager manager;
 
+    // Add ent_array for different types
+    manager.add_array<int>();
+    manager.add_array<float>();
 
+    // Get and use ent_array<int>
+    auto& int_array = manager.get_array<int>();
+    ent_handle int_handle = int_array.insert(42);
+    std::cout << "Inserted int: " << int_array.get(int_handle) << std::endl;
+
+    // Get and use ent_array<float>
+    auto& float_array = manager.get_array<float>();
+    ent_handle float_handle = float_array.insert(3.14f);
+    std::cout << "Inserted float: " << float_array.get(float_handle) << std::endl;
+
+    return 0;
 }
