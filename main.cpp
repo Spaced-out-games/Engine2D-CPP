5411ef2988c22aad76f60ad52eb7ea32875a20c1
Devin Frost <devin.frost4311@gmail.com>
2024-07-01 16:45:59 -0400

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "shared_types.h"
+
+int main()
+{
+	//ent_chunk<int> integers;
+	//const bitset_512& the_bitset = integers.getStates();
+	//std::cout << the_bitset.get(0);
+	//integers.insert(42);
+	//std::cout << the_bitset.get(0);
+
+
+}
