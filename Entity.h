97869f468feeb323aece4025c7366dc7e5068b54
Devin Frost <devin.frost4311@gmail.com>
2024-07-01 23:19:26 -0400

    Removed some bloat (haha)

--- Entity.h
+++ Entity.h
@@ -3,36 +3,15 @@
 
 #include <glm/glm.hpp>
 #include <cstdint>
-#include "shared_types.h"
+#include "ent_handle.h"
 
-/*
-    Basic entity class. It's got no real behavior, just provides an interface for extended types to keep track of their ref counts, as well as an interface for the engine to check if the Entity is being referenced
-    so that it can be moved to the appropriate slot by the engine, ensuring stability for objects that are referenced.
-
-*/
-
-class Entity : public std::enable_shared_from_this<Entity>
+class Entity
 {
-public:
-    Entity() = default;
+	// It is very important that you get references directly from the Entities themselves so that their reference counts are up-to-date
+	ent_handle getReference();
 
-    // Method to get a shared pointer to itself
-    std::shared_ptr<Entity> getSharedPtr()
-    {
-        return shared_from_this();
-    }
-
-    // Method to get the reference count
-    long getReferenceCount() const
-    {
-        return shared_ptr.use_count();
-    }
-
-
-
-private:
-
-    std::shared_ptr<Entity> shared_ptr; // Holds the shared pointer to itself
 };
+
+
 
 #endif
