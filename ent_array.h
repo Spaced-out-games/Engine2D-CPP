51a0bb6847089378a5988dc93fb8f83f0858798a
Devin Frost <devin.frost4311@gmail.com>
2024-07-01 22:31:17 -0400

    Implemented a compression algorithm. Just saving progress in case new changes are worse

--- ent_array.h
+++ ent_array.h
@@ -63,6 +63,14 @@ public:
         }
     }
 
+    // Fallback option
+    void remove(int index)
+    {
+        int block_ID = index / 512;
+        index = index % 512;
+        blocks[block_ID].remove(index);
+    }
+
     // Get the total number of entities across all blocks
     bitset_size_t size() const
     {
@@ -86,6 +94,104 @@ public:
         blocks.clear();
     }
 
+
+
+    void compress()
+    {
+        // The block index of the left pointer
+        size_t leftmost_block_index = 0;
+
+        // The left pointer block index
+        size_t left_index = 0;
+
+        // The block index of the right pointer
+        size_t rightmost_block_index = size() - 1;
+
+        // The right pointer block index
+        size_t right_index = 0;
+
+        // Find the leftmost block that isn't full
+        while (!blocks[leftmost_block_index].isFull() && leftmost_block_index < blocks.size())
+        {
+            leftmost_block_index++;
+        }
+
+        // If we got to the end from the left and there wasnt any empty slots, abort
+        if (leftmost_block_index == blocks.size() && !blocks[leftmost_block_index -1].isFull()) { return; }
+
+        // Find the rightmost block that has contents
+        while (blocks[rightmost_block_index].isEmpty() && rightmost_block_index >= 0)
+        {
+            rightmost_block_index--;
+        }
+        // If we got to the beginning of the array and it has no contents, abort
+        if (rightmost_block_index == -1 && blocks[0].isEmpty()) { return; }
+
+
+
+        // Now that we have the correct starting blocks, find the correct indices
+        while (leftmost_block_index < rightmost_block_index)
+        {
+            // Get the index of the first empty element in the leftmost block
+            left_index = blocks[leftmost_block_index].getFirstEmpty();
+
+            // Get the index of the last occupied element of the rightmost block
+            right_index = blocks[rightmost_block_index].getLastFull();
+
+            // Move either block inward if necessary
+
+            // There are no empty spaces on the left
+            while (left_index == -1)
+            {
+                // So move inward from the left
+                leftmost_block_index++;
+                left_index = blocks[leftmost_block_index].getFirstEmpty();
+            }
+
+            // There are no occupied spaces on the right
+            while (right_index == -1)
+            {
+                rightmost_block_index--;
+                right_index = blocks[rightmost_block_index].getLastFull();
+            }
+
+            // Now that we have verified we have valid block indices, move from the right to the left
+
+            // Insert a copy of the rightmost to the leftmost slot
+            blocks[leftmost_block_index].insert(blocks[rightmost_block_index].get_entity(right_index));
+
+            // Update the state of the rightmost
+            blocks[rightmost_block_index].get_bitset().set(right_index, 0);
+
+            // And update the state of the leftmost
+            blocks[leftmost_block_index].get_bitset().set(left_index, 1);
+
+
+        }
+
+
+    }
+
+    void add_block() {
+        blocks.emplace_back();
+    }
+
+    //template <typename T>
+    void print_blocks() {
+        for (size_t i = 0; i < blocks.size(); ++i) {
+            std::cout << "Block " << i << ": ";
+            for (int j = 0; j < 512; ++j) {
+                if (blocks[i].get_bitset().get(j)) {
+                    std::cout << blocks[i].get_entity(j) << " ";
+                }
+                else
+                {
+                    std::cout << "(EMPTY SLOT)";
+                }
+            }
+            std::cout << std::endl;
+        }
+    }
 private:
     std::vector<ent_block<T>> blocks; // Vector of blocks
 };
