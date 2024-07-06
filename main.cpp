5d5d7b4609c381e4b1290eba7310e0a2fdaa721a
Devin Frost <devin.frost4311@gmail.com>
2024-07-05 20:36:11 -0400

    Removed test code

--- main.cpp
+++ main.cpp
@@ -2,43 +2,6 @@
 #include <iostream>
 #include <random>
 int main() {
-    // Create an ent_array of integers
-    ent_array<int> array;
-
-    // Add blocks to the array
-    array.add_block();
-    array.add_block();
-    //array.add_block();
-
-    // Populate blocks with integers
-    for (int i = 0; i < 1000; ++i) {
-        array.insert(i);
-    }
-    for (size_t i = 0; i < 92; i++)
-    {
-        array.remove(rand() % 1000);
-    }
-
-    
-
-    std::cout << "Before compression:" << std::endl;
-    //array.print_blocks();
-    array.print_blocks();
-    std::cout << '\n';
-    //array.print_bitset(1);
-    
-
-    std::cout << "Compressing...";
-    // Perform compression
-    array.compress();
-    std::cout << "Done!" << std::endl;
-
-
-
-    std::cout << "After compression:" << std::endl;
-    array.print_blocks();
-    std::cout << '\n';
-    //array.print_bitset(1);
 
     return 0;
 }
