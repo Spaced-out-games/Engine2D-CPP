2c87dd02f6cdda4740304627d2d8732c660af4c1
Devin Frost <devin.frost4311@gmail.com>
2024-07-13 10:23:16 -0400

    Trying to get controller to work by using SDL_Event* instead of SDL_Event, since openGL's state caused a segfault. Now that it's fixed, can be removed

--- Controller.h
+++ Controller.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <SDL.h>
 #include <iostream>
-//if (events.type == SDL_QUIT || events.type == SDL) { engineCore::quit(); }
+
 class engineCore;
 
 class Controller
@@ -11,22 +11,18 @@ public:
     ~Controller();
 
 private:
-    // SDL event
-    SDL_Event events;
-
-    // Whether the controller is actively polling an event
+    SDL_Event* events;
     bool isPollingEvents;
 
-    // Let engineCore access Controller's private members and methods
     friend engineCore;
 
-    // This is called when the engine receives an event. 
     void inputEvent();
+    void enablePollingEvents();
+    void disablePollingEvents();
 
-    // For inherited types, overwrite these methods
     virtual void onQuit() { engineCore::quit(); };
     virtual void onKeyDown() { };
-    virtual void onKeyUp() {  };
+    virtual void onKeyUp() { };
     virtual void onMouseMove() { };
     virtual void onMouseButtonDown() {};
     virtual void onMouseButtonUp() { };
@@ -39,79 +35,75 @@ private:
 
 #include "engineCore.h"
 
-Controller::Controller() : isPollingEvents(false) {}
+Controller::Controller() : isPollingEvents(false), events(nullptr) 
+{
+    events = new SDL_Event();
+}
 
-Controller::~Controller() = default;
+Controller::~Controller() {
+    delete events;
+}
 
 void Controller::inputEvent()
 {
-    switch (events.type)
+    switch (events->type)
     {
     case SDL_QUIT:
         onQuit();
-        break; 
-
+        break;
     case SDL_KEYUP:
         onKeyUp();
-        break; 
-
+        break;
     case SDL_KEYDOWN:
         onKeyDown();
-        break; 
-
+        break;
     case SDL_MOUSEMOTION:
         onMouseMove();
-        break; 
-
+        break;
     case SDL_MOUSEBUTTONDOWN:
         onMouseButtonDown();
-        break; 
-
+        break;
     case SDL_MOUSEBUTTONUP:
         onMouseButtonUp();
-        break; 
-
+        break;
     case SDL_MOUSEWHEEL:
         onMouseWheelMove();
-        break; 
-
+        break;
     case SDL_DROPFILE:
         onFileDrop();
-        break; 
-
+        break;
     case SDL_DROPTEXT:
         onTextDrop();
-        break; 
-
+        break;
     case SDL_DROPBEGIN:
         onBeginDrop();
-        break; 
-
+        break;
     case SDL_DROPCOMPLETE:
         onEndDrop();
-        break; 
-
+        break;
     case SDL_APP_WILLENTERBACKGROUND:
         std::cout << "App will enter background\n";
-        //onApplicationWillEnterBackground();
         break;
     case SDL_APP_DIDENTERBACKGROUND:
         std::cout << "App entered background\n";
-
-        //onApplicationDidEnterBackground();
         break;
     case SDL_APP_WILLENTERFOREGROUND:
         std::cout << "App will enter foreground\n";
-
-        //onApplicationWillEnterForeground();
         break;
     case SDL_APP_DIDENTERFOREGROUND:
         std::cout << "App entered foreground\n";
-
-        //onApplicationDidEnterForeground();
         break;
     default:
-        //std::cerr << "Unsupported Event type: Ignoring..." << std::endl; // Added missing semicolon
-        break; 
+        break;
     }
+}
+
+void Controller::enablePollingEvents()
+{
+    isPollingEvents = true;
+}
+
+void Controller::disablePollingEvents()
+{
+    isPollingEvents = false;
 }
