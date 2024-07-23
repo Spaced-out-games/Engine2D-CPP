061ccb5831cadd68cc15bde2d3e1958a4b0d31d4
Devin Frost <devin.frost4311@gmail.com>
2024-07-23 00:45:50 -0400

    Controller now directly holds an SDL_event instead of a pointer to one. Added a default onWindowResize function Added getMOusePosition() and getNormalizeMousePos()

--- Controller.h
+++ Controller.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <SDL.h>
 #include <iostream>
-
+#include "Camera.h"
 class engineCore;
 
 class Controller
@@ -9,9 +9,9 @@ class Controller
 public:
     Controller();
     ~Controller();
-
+    SDL_Event events;
 private:
-    SDL_Event* events;
+
     bool isPollingEvents;
 
     friend engineCore;
@@ -20,66 +20,84 @@ private:
     void enablePollingEvents();
     void disablePollingEvents();
 
-    virtual void onQuit() { engineCore::quit(); };
-    virtual void onKeyDown() { };
-    virtual void onKeyUp() { };
-    virtual void onMouseMove() { };
-    virtual void onMouseButtonDown() {};
-    virtual void onMouseButtonUp() { };
-    virtual void onMouseWheelMove() {};
-    virtual void onFileDrop() {  };
-    virtual void onTextDrop() {  };
-    virtual void onBeginDrop() { };
-    virtual void onEndDrop() {  };
+    virtual void onQuit(SDL_Event event) { engineCore::quit(); };
+    virtual void onKeyDown(SDL_Event event) { };
+    virtual void onKeyUp(SDL_Event event) { };
+    virtual void onMouseMove(SDL_Event event) { getNormalizedMousePos(); };
+    virtual void onMouseButtonDown(SDL_Event event) {};
+    virtual void onMouseButtonUp(SDL_Event event) { };
+    virtual void onMouseWheelMove(SDL_Event event) {};
+    virtual void onFileDrop(SDL_Event event) {  };
+    virtual void onTextDrop(SDL_Event event) {  };
+    virtual void onBeginDrop(SDL_Event event) { };
+    virtual void onEndDrop(SDL_Event event) {  };
+    virtual void onWindowResize(SDL_Event event){
+        glViewport(0, 0, event.window.data1, event.window.data2);
+    };
+    glm::vec2 getMousePosition() {
+        int x, y;
+        SDL_GetMouseState(&x, &y);
+        return glm::vec2(static_cast<float>(x), static_cast<float>(y));
+    }
+
+    glm::vec2 getNormalizedMousePos()
+    {
+        int width;
+        int height;
+        SDL_GetWindowSize(SDL_GL_GetCurrentWindow(), &width, &height);
+        glm::vec2 mousepos = getMousePosition() - glm::vec2(width / 2, height / 2);
+        mousepos = (mousepos / glm::vec2(width, height));
+
+        //std::cout <<'(' << mousepos.x << ", " << mousepos.y << ")\t";
+
+        return mousepos;
+    }
 };
 
 #include "engineCore.h"
 
-Controller::Controller() : isPollingEvents(false), events(nullptr) 
-{
-    events = new SDL_Event();
-}
+Controller::Controller() : isPollingEvents(false)
+{}
 
 Controller::~Controller() {
-    delete events;
-}
+};
 
 void Controller::inputEvent()
 {
-    switch (events->type)
+    switch (events.type)
     {
     case SDL_QUIT:
-        onQuit();
+        onQuit(events);
         break;
     case SDL_KEYUP:
-        onKeyUp();
+        onKeyUp(events);
         break;
     case SDL_KEYDOWN:
-        onKeyDown();
+        onKeyDown(events);
         break;
     case SDL_MOUSEMOTION:
-        onMouseMove();
+        onMouseMove(events);
         break;
     case SDL_MOUSEBUTTONDOWN:
-        onMouseButtonDown();
+        onMouseButtonDown(events);
         break;
     case SDL_MOUSEBUTTONUP:
-        onMouseButtonUp();
+        onMouseButtonUp(events);
         break;
     case SDL_MOUSEWHEEL:
-        onMouseWheelMove();
+        onMouseWheelMove(events);
         break;
     case SDL_DROPFILE:
-        onFileDrop();
+        onFileDrop(events);
         break;
     case SDL_DROPTEXT:
-        onTextDrop();
+        onTextDrop(events);
         break;
     case SDL_DROPBEGIN:
-        onBeginDrop();
+        onBeginDrop(events);
         break;
     case SDL_DROPCOMPLETE:
-        onEndDrop();
+        onEndDrop(events);
         break;
     case SDL_APP_WILLENTERBACKGROUND:
         std::cout << "App will enter background\n";
@@ -93,6 +111,9 @@ void Controller::inputEvent()
     case SDL_APP_DIDENTERFOREGROUND:
         std::cout << "App entered foreground\n";
         break;
+    case SDL_WINDOWEVENT_RESIZED:
+        onWindowResize(events);
+        std::cout << "Window resized\n";
     default:
         break;
     }
@@ -107,3 +128,45 @@ void Controller::disablePollingEvents()
 {
     isPollingEvents = false;
 }
+/*
+
+class CameraController : public Controller
+{
+public:
+    CameraController()
+        : camera(glm::vec3(0.0f, 0.0f, 3.0f)) // Initializing Camera with a default position
+    {
+        // Set initial projection matrix if needed
+        camera.setProjection(45.0f, 16.0f / 9.0f, 0.1f, 100.0f);
+        camera.setRotation(0.0f, glm::radians(-90.0f));
+        //camera.setUniformLocations(shaderProgramID); // You need to set shaderProgramID
+    }
+
+    void onKeyDown(SDL_Event event) override
+    {
+        if (event.key.keysym.sym == SDLK_d) {
+            camera.setPosition(camera.getPosition() + camera.getRight() * cameraSpeed);
+        }
+        if (event.key.keysym.sym == SDLK_a) {
+            camera.setPosition(camera.getPosition() - camera.getRight() * cameraSpeed);
+        }
+        if (event.key.keysym.sym == SDLK_w) {
+            camera.setPosition(camera.getPosition() + camera.getFront() * cameraSpeed);
+        }
+        if (event.key.keysym.sym == SDLK_s) {
+            camera.setPosition(camera.getPosition() - camera.getFront() * cameraSpeed);
+        }
+    }
+
+    void updateCamera()
+    {
+        camera.updateUniforms();
+    }
+    Camera camera;
+private:
+    
+    float cameraSpeed = 0.05f; // Adjust speed as needed
+    GLuint shaderProgramID; // Set this to the actual shader program ID
+};
+
+*/
