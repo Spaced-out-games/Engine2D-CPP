a2abfafd7e475637f6c71507a5d629e1749d4340
Devin Frost <devin.frost4311@gmail.com>
2024-07-25 11:57:36 -0400

    Massive overhaul on the engineCore. engineCore is now Application, and custom state is handled through a templated type.
    Custom logic can be added more directly by assigning function pointers to tick() and bootstrap(). Mostly for prototyping, and will be replaced with virtual functions at a later date

--- engineCore.h
+++ engineCore.h
@@ -1,75 +1,63 @@
 #pragma once
 #define SDL_MAIN_HANDLED
+
 #include <SDL.h>
 #include <GL/glew.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 #include <string>
 #include <cstdlib>
-#include "ent_manager.h"
 #include <iostream> // For std::cerr
-#include "shader_manager.h"
-#include "global_uniforms.h"
-#include "Camera.h"
-#include "Mesh2D.h"
-#include "GUI_Node.h"
 
-
-#define compare_s(command, test) (strncmp(command, test, strlen(command)) == 0)
-
-typedef void* (*FunctionPointer)(void*);
-class CLArg;
 class Controller;
 
-void drawTriangleImmediateMode();
-void drawTriangle(SDL_Window* window);
-std::string readShaderFile(const std::string& filePath);
-GLuint compileShader(const std::string& source, GLenum shaderType);
-GLuint createShaderProgram(const std::string& vertexPath, const std::string& fragmentPath);
-
-class engineCore
+template <class state_t>
+class Application
 {
 public:
-    // temporary testing
+    using app_callback = void (*)(void*);
 
+    Application();
+    ~Application();
 
-    static void quit();
-    static bool isRunning();
-    virtual int main();
+    // Quits the application
+    void quit();
+
+    // Returns whether the engine is currently running
+    bool isRunning();
+
+    // Main run loop
+    virtual int run();
+
+    // Different tick functions for various subsystems
     virtual void graphics_tick();
     virtual void phys_tick();
     virtual void IO_tick();
     virtual void logic_tick();
-    static bool init_core(int argc, char* argv[]);
-    static engineCore*& getEngineCore();
-    SDL_GLContext& getContext();
-    virtual void bootstrap();
-    ent_manager& getManager();
-    double getDeltaTime() const;
-    void enableVsync();
-    void disableVsync();
-    bool isVsyncEnabled() const;
-    void setWindowDimensions(int newWidth, int newHeight);
-    //void registerController(Controller* controller);
-    bool initialized() { return init_success; }
-    SDL_Window* getWindow();
-    shader_manager shaderManager;
-    Shader& getShader(size_t index) { return shaders[index]; }
-protected:
-    engineCore();
-    virtual ~engineCore();
-    std::vector <Shader> shaders;
-private:
-    friend CLArg;
 
+    // Getters
+    SDL_GLContext& getContext();
+    double getDeltaTime();
+    SDL_Window* getWindow();
+    bool is_vsync_enabled();
+
+    // Setters
+    void enable_vsync();
+    void disable_vsync();
+    void setWindowDimensions(int newWidth, int newHeight);
+    void setController(Controller* new_controller);
+    void setCustomState(const state_t& state);
+
+    void set_tick_function(app_callback func);
+    void set_bootstrap_function(app_callback func);
+
+    // State management
+    state_t* getCustomState();
+
+private:
     void init_SDL();
 
-    ent_manager EntityManager;
     SDL_Window* sdlWindow;
     SDL_GLContext glContext;
     SDL_Event sdlEvents;
-
     int mWidth;
     int mHeight;
     double deltaTime;
@@ -77,29 +65,41 @@ private:
     bool running;
     bool init_success;
     bool mFullscreen;
+    app_callback bootstrap;
+    app_callback tick;
+
     std::string mTitle;
     Controller* controller;
-    static engineCore* instance;
-    static void parse_arg(char* arg, char* target_command, CLArg& CLArgument);
-    void setUniform(int s, const std::string& name, const glm::mat4& mat);
+    state_t custom_state;
 };
 
-#include "CLArgument.h"
+// Implementation of the template functions
 #include "Controller.h"
 
-engineCore* engineCore::instance = nullptr;
-//shader_manager* engineCore::shaderManager = shader_manager::getInstance();
-
-engineCore::engineCore()
-    : sdlWindow(nullptr), mWidth(1920), mHeight(1080), deltaTime(0.0),
-    vsync_enabled(false), running(false), init_success(false), mFullscreen(false), mTitle("Engine"), controller(nullptr)
+template <class state_t>
+Application<state_t>::Application() :
+    sdlWindow(nullptr),
+    glContext(nullptr),
+    mWidth(1920),
+    mHeight(1080),
+    deltaTime(0.0),
+    vsync_enabled(false),
+    running(false),
+    init_success(false),
+    mFullscreen(false),
+    mTitle("Engine"),
+    controller(nullptr),
+    bootstrap(nullptr),
+    tick(nullptr)
 {
-    instance = this;
-    
-
+    #ifdef _DEBUG
+    std::cout << "engineCore constructed\n";
+    #endif
+    init_SDL();
 }
 
-engineCore::~engineCore()
+template <class state_t>
+Application<state_t>::~Application()
 {
     if (glContext)
     {
@@ -114,9 +114,9 @@ engineCore::~engineCore()
     SDL_Quit();
 }
 
-
-
-void engineCore::init_SDL() {
+template <class state_t>
+void Application<state_t>::init_SDL()
+{
     // Initialize SDL. In order
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
@@ -163,25 +163,153 @@ void engineCore::init_SDL() {
 
     // Set the clear color
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+    std::cout << "1. Set up SDL and OpenGL context.\n";
 }
 
-
-void engineCore::quit()
+template <class state_t>
+void Application<state_t>::quit()
 {
-    getEngineCore()->running = false;
+    running = false;
 }
 
-bool engineCore::isRunning()
+template <class state_t>
+bool Application<state_t>::isRunning()
 {
-    return getEngineCore()->running;
+    return running;
 }
 
-void engineCore::graphics_tick()
-{
+template <class state_t>
+void Application<state_t>::graphics_tick() {}
 
+template <class state_t>
+void Application<state_t>::phys_tick() {}
+
+template <class state_t>
+void Application<state_t>::IO_tick()
+{
+    if (controller)
+    {
+        controller->enablePollingEvents();
+        while (SDL_PollEvent(&controller->events)) // This is where the fun happens
+        {
+            controller->inputEvent();
+        }
+        controller->disablePollingEvents();
+    }
+    else
+    {
+        std::cerr << "ERROR: No controller connected! Using fallback option (basic Controller) \n";
+        controller = new Controller();
+    }
+}
+
+template <class state_t>
+void Application<state_t>::logic_tick() {}
+
+template <class state_t>
+int Application<state_t>::run()
+{
+    // bootstrap if we can, passing a pointer to the application
+    if (bootstrap) {
+        bootstrap(this);
+    }
+
+    #ifdef _DEBUG
+    std::cout << "engineCore bootstrapped\n";
+    #endif
+
+
+    running = true;
+    while (running)
+    {
+
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Tick the engine, passing a pointer to the application
+        tick(this);
+        #ifdef _DEBUG
+        std::cout << "engineCore tick\n";
+        #endif
+        SDL_GL_SwapWindow(sdlWindow);
+
+        // logic_tick();
+    }
+    return 0;
+}
+
+template <class state_t>
+SDL_GLContext& Application<state_t>::getContext()
+{
+    return glContext;
+}
+
+template <class state_t>
+void Application<state_t>::enable_vsync()
+{
+    SDL_GL_SetSwapInterval(1);
+    vsync_enabled = true;
+}
+
+template <class state_t>
+void Application<state_t>::disable_vsync()
+{
+    SDL_GL_SetSwapInterval(0);
+    vsync_enabled = false;
+}
+
+template <class state_t>
+bool Application<state_t>::is_vsync_enabled()
+{
+    return vsync_enabled;
+}
+
+template <class state_t>
+void Application<state_t>::setWindowDimensions(int newWidth, int newHeight)
+{
+    mWidth = newWidth;
+    mHeight = newHeight;
+    SDL_SetWindowSize(sdlWindow, mWidth, mHeight);
+    glViewport(0, 0, mWidth, mHeight);
+}
+
+template <class state_t>
+SDL_Window* Application<state_t>::getWindow()
+{
+    return sdlWindow;
+}
+
+template <class state_t>
+void Application<state_t>::setController(Controller* new_controller)
+{
+    controller = new_controller;
+}
+
+template <class state_t>
+void Application<state_t>::setCustomState(const state_t& state)
+{
+    custom_state = state;
+}
+
+template <class state_t>
+state_t* Application<state_t>::getCustomState()
+{
+    return &custom_state;
+}
+
+template <class state_t>
+void Application<state_t>::set_bootstrap_function(app_callback func)
+{
+    bootstrap = func;
+}
+
+template <class state_t>
+void Application<state_t>::set_tick_function(app_callback func)
+{
+    tick = func;
 }
 /*
-
 void render(model& m, glm::vec3 color, glm::mat4& transform) {
 
     glUseProgram(m.shaderProgram);
@@ -196,185 +324,3 @@ void render(model& m, glm::vec3 color, glm::mat4& transform) {
 }
 
 */
-
-void engineCore::IO_tick()
-{
-
-    if (controller)
-    {
-        controller->enablePollingEvents();
-
-
-        
-        while (SDL_PollEvent(&controller->events)) // This is where the fun happens
-        {
-            controller->inputEvent();
-        }
-        controller->disablePollingEvents();
-    }
-    else
-    {
-        std::cerr << "ERROR: No controller connected! Using fallback option (basic Controller) \n";
-        controller = new Controller();
-    }
-}
-
-void engineCore::phys_tick() {}
-
-void engineCore::bootstrap()
-{
-    controller = &Controller();
-}
-
-void engineCore::logic_tick()
-{
-
-}
-
-int engineCore::main()
-{
-
-    // last
-    bootstrap();
-
-    //GUINode(glm::vec3 initial_translation, glm::vec3 initial_scale, glm::vec3 initial_color)
-    GUINode root(glm::vec3(0, 0, 0), glm::vec3(1, 1, 1), glm::vec3(1, 0, 0), 1);
-    GUINode child(glm::vec3(0.0, 0.0, 0), glm::vec3(0.5, 0.5, 0.5), glm::vec3(0, 1, 0), 1);
-    GUINode child2(glm::vec3(0.0, 0.0, 0), glm::vec3(0.25, 0.25, 0.25), glm::vec3(0, 0, 1), 0);
-
-    root.addChild(&child);
-    child.addChild(&child2);
-
-    glm::vec3 velocity = { 0.000000,0.000000,0.000000 };
-
-    GUINode::init();
-    Shader::getDefaultShader().use();
-    float dy = 0.0f, dp = 0.0f;
-    running = true;
-    while (running)
-    {
-        // Maybe one thread for each?
-        //IO_tick();
-        phys_tick();
-        root.translation += velocity;
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        //controller->camera.setRotation(dy, dp);
-        //controller->camera.updateUniforms();
-        root.draw();
-        root.rotation+= 0.02;
-        child.rotation += 0.002;
-        child2.rotation -= 0.002;
-
-        SDL_GL_SwapWindow(sdlWindow);
-
-        logic_tick();
-
-        
- 
-    }
-    return 0;
-}
-
-bool engineCore::init_core(int argc, char* argv[])
-{
-    // Simply creates an instance if it can
-    if (instance == nullptr)
-    {
-        // Create an instance if we dont have one
-        instance = new engineCore();
-
-        // Run Command-Line arguments
-        for (int i = 1; i < argc; i++)
-        {
-            CLArg argument;
-            parse_arg(argv[i], "title", argument);
-            
-            if (argument.isValid()) {
-                argument.execute(instance);
-            }
-        }
-        instance->init_SDL();
-        return true;
-    }
-    return false;
-}
-
-void engineCore::parse_arg(char* arg, char* target_command, CLArg& CLArgument)
-{
-    if (strlen(arg) < 2 || target_command == nullptr || arg == nullptr) return;
-
-    if (strlen(arg) == 2 && arg[0] == '-')
-    {
-        CLArgument.mCommand = arg + 1;
-        CLArgument.mArgument = nullptr;
-        return;
-    }
-
-    if (strlen(arg) > 2 && arg[0] == '-' && arg[1] == '-')
-    {
-        char* command = arg + 2;
-        if (strncmp(command, target_command, strlen(target_command)) == 0)
-        {
-            char* argument = command + strlen(target_command);
-            CLArgument.mCommand = target_command;
-            CLArgument.mArgument = argument;
-            return;
-        }
-    }
-}
-
-engineCore*& engineCore::getEngineCore()
-{
-    return instance;
-}
-
-SDL_GLContext& engineCore::getContext()
-{
-    return glContext;
-}
-
-ent_manager& engineCore::getManager()
-{
-    return EntityManager;
-}
-
-double engineCore::getDeltaTime() const
-{
-    return deltaTime;
-}
-
-void engineCore::enableVsync()
-{
-    vsync_enabled = true;
-    SDL_GL_SetSwapInterval(1);
-}
-
-void engineCore::disableVsync()
-{
-    vsync_enabled = false;
-    SDL_GL_SetSwapInterval(0);
-}
-
-bool engineCore::isVsyncEnabled() const
-{
-    return vsync_enabled;
-}
-
-void engineCore::setWindowDimensions(int newWidth, int newHeight)
-{
-    mWidth = newWidth;
-    mHeight = newHeight;
-    SDL_SetWindowSize(sdlWindow, mWidth, mHeight);
-}
-
-//void engineCore::registerController(Controller* newController)
-//{
-//    controller = newController;
-//}
-SDL_Window* engineCore::getWindow()
-{
-    return sdlWindow;
-}
-
-
