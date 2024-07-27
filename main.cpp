74c53c8faacbffbdf5302aa03886c5f966855ae5
Devin Frost <devin.frost4311@gmail.com>
2024-07-27 00:45:25 -0400

    Upgraded to use Application and VEditorController Updated some more logic for testing purposes

--- main.cpp
+++ main.cpp
@@ -31,22 +31,29 @@ int main(int argc, char* argv[]) {
 #ifdef DEMO_DEBUG
 #define SDL_MAIN_HANDLED
 
-#include "engineCore.h"
+#pragma once
+#include "Application.h"
 #include "GUI_Node.h"
 #include "shader.h"
 #include "shader_manager.h"
+#include "VEditorController.h"
 using glm::vec3;
 using glm::mat4;
 
 struct windowContent
 {
     std::vector<GUINode> nodes;
-    std::vector<std::string> argv;
+    size_t num_ticks;
     Shader shader;
+    VEditorController<windowContent> controller;
 
-
-    // Default constructor
-    windowContent() = default; // Use default constructor for the default case
+    windowContent()
+        : nodes(), shader(), controller(nullptr) // Initialize controller with app
+    {}
+    // Constructor that initializes all members
+    windowContent(Application<windowContent>* app)
+        : nodes(), shader(), controller(app) // Initialize controller with app
+    {}
 };
 
 template <class state_t>
@@ -61,21 +68,54 @@ void custom_bootstrap(void* input)
 
     // Create the geometry
     GUINode::init();
-    app->getCustomState()->nodes.push_back(GUINode(vec3(0.0f,0.0f,0.0f),vec3(1.0f,1.0f, 1.0f), vec3(1.0f, 1.0f, 1.0f), 0));
+    app->getCustomState()->nodes.push_back(GUINode(vec3(0.0f,0.0f,0.0f),vec3(1.0f,1.0f, 1.0f), vec3(1.0f, 1.0f, 1.0f), 1));
+    app->getCustomState()->nodes[0].addChild(new GUINode(vec3(0.0f, 0.0f, 0.0f), vec3(0.5f, 0.5f, 0.5), vec3(0.0f, 1.0f, 1.0f), 1));
 }
 
 
 template <class state_t>
 void custom_tick(void* input)
 {
+    
+
     // cast to application pointer
     Application<state_t>* app = (Application<state_t>*)input;
+    while (SDL_PollEvent(&(app->getCustomState()->controller.getEvents())))
+    {
+        app->getCustomState()->controller.enablePollingEvents();
+        app->getCustomState()->controller.inputEvent();
+        app->getCustomState()->controller.disablePollingEvents();
+
+    };
 
     // use the shader
     app->getCustomState()->shader.use(); 
 
     // draw the node
     app->getCustomState()->nodes[0].draw();
+
+    glm::vec2 mouse_position = app->getCustomState()->controller.getNormalizedMousePos();
+    glm::vec3 root_scale = app->getCustomState()->nodes[0].scaleFromMatrix(app->getCustomState()->nodes[0].get_nested_transform());
+    glm::vec3 child_scale = app->getCustomState()->nodes[0].children[0]->scaleFromMatrix(app->getCustomState()->nodes[0].children[0]->get_nested_transform());
+    
+    //std::cout << "Scale: " << child_scale.x << ", " << child_scale.y << ", " << child_scale.z << std::endl;
+
+    auto collision = app->getCustomState()->nodes[0].collides(mouse_position);
+    if (app->getCustomState()->num_ticks % 1000 == 0)
+    {
+        std::cout << collision << '\n';
+
+    }
+    if (collision)
+    {
+        //std::cout << collision->occupied_slots;
+        //collision->onCollide(nullptr);
+    }
+    app->getCustomState()->num_ticks++;
+}
+
+void collision_check(windowContent& content)
+{
 }
 
 
@@ -84,10 +124,12 @@ void custom_tick(void* input)
 int main(int argc, char* argv[])
 {
     Application<windowContent> app (argc, argv); // initializes SDL window & context
-    windowContent content; // Adds arguments to the state
+    
+    
+
+    // Initialize windowContent with the Application instance
+    windowContent content(&app);
     app.setCustomState(content); // assigns state
-
-
 
     // set but not call functions
     app.set_bootstrap_function(custom_bootstrap<windowContent>);
