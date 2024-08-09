d5e22ee485b6e5385a8877fda6acc6b6e106bfb7
Devin Frost <devin.frost4311@gmail.com>
2024-08-09 01:25:44 -0400

    Moved initialization into its own logical step so that empty shader initialization is legal and stable Made use() have debug information printed only when in debug mode Made uniform setting more verbose Added new methods that return source code for the default shaders Simplified default shader code for debuggin purposes

--- shader.h
+++ shader.h
@@ -1,22 +1,36 @@
-#include <GL/glew.h>       // For OpenGL functions and types
-#include <iostream>        // For std::cerr
-#include <string>          // For std::string (if you use strings for shader source)
-
 #ifndef SHADER_H
 #define SHADER_H
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <string> 
+
 struct Shader
 {
-    // for now, everything is public
     GLuint ShaderProgram;
-    Shader() {
-        ShaderProgram = 0;
-        #ifdef _DEBUG
-        std::cout << "2. Create a shader either from a file or from source code. (Note: Constructed invalid shader)\n";
 
-        #endif // DEBUG
+    Shader() : ShaderProgram(0) {
 
     }
-    Shader(const char* vertexShaderSource, const char* fragmentShaderSource) { ShaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource); std::cout << "2. Create a shader either from a file or from source code.\n"; }
+
+    Shader(const char* vertexShaderSource, const char* fragmentShaderSource) {
+        init(vertexShaderSource, fragmentShaderSource);
+    }
+
+    void init(const char* vertexShaderSource, const char* fragmentShaderSource)
+    {
+        ShaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
+        std::cout << "2. Create a shader either from a file or from source code.\n";
+    }
+
+    ~Shader() {
+        std::cout << "Destroyed shader with ID: " << ShaderProgram;
+        if (ShaderProgram != 0) {
+            glDeleteProgram(ShaderProgram);
+        }
+    }
 
     GLuint compileShader(GLenum ShaderType, const char* ShaderSource) {
         GLuint shader = glCreateShader(ShaderType);
@@ -34,7 +48,6 @@ struct Shader
         return shader;
     }
 
-    // Function to create shader program
     GLuint createShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource) {
         GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
         GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
@@ -58,90 +71,77 @@ struct Shader
         return program;
     }
 
-    void use() const
-    {
-        #ifdef DEBUG
+    void use() const {
+        #ifdef _DEBUG
         std::cout << "5. Use the shader associated with this Mesh3D by calling glUseProgram with the shader's program ID.\n";
-        #endif // DEBUG
-
+        #endif
         glUseProgram(ShaderProgram);
     }
-    static void setUniform(const char* uniform_ID, const glm::mat4& matrix4)
-    {
-        GLint program_int;
-        glGetIntegerv(GL_CURRENT_PROGRAM, &program_int);
-        GLuint program_uint = static_cast<GLuint>(program_int);
-        GLint uniformCode = glGetUniformLocation(program_uint, uniform_ID);
 
+    void setUniform(const char* uniform_ID, const glm::mat4& matrix4) const {
+        GLint uniformCode = glGetUniformLocation(ShaderProgram, uniform_ID);
         #ifdef _DEBUG
-        if (uniformCode != -1) // Check if the uniform location is valid
-        {
+        if (uniformCode != -1) {
+            std::cout << "6. Set any required uniforms.\n";
             glUniformMatrix4fv(uniformCode, 1, GL_FALSE, glm::value_ptr(matrix4));
         }
-        else
-        {
-            std::cerr << "Warning: Uniform '" << uniform_ID << "' not found in shader program " << program_uint << std::endl;
+        else {
+            std::cerr << "WARNING: Uniform '" << uniform_ID << "' not found in shader program " << ShaderProgram << std::endl;
         }
         #else
-        // In release builds, just set the uniform without checking
         glUniformMatrix4fv(uniformCode, 1, GL_FALSE, glm::value_ptr(matrix4));
         #endif
     }
 
-    static void setUniform(const char* uniform_ID, const glm::vec3& vector3)
-    {
-        GLint program_int;
-        glGetIntegerv(GL_CURRENT_PROGRAM, &program_int);
-        GLuint program_uint = static_cast<GLuint>(program_int);
-        GLint uniformCode = glGetUniformLocation(program_uint, uniform_ID);
-
+    void setUniform(const char* uniform_ID, const glm::vec3& vector3) const {
+        GLint uniformCode = glGetUniformLocation(ShaderProgram, uniform_ID);
         #ifdef _DEBUG
-        if (uniformCode != -1) // Check if the uniform location is valid
-        {
+        if (uniformCode != -1) {
+            std::cout << "6. Set any required uniforms.\n";
             glUniform3f(uniformCode, vector3.x, vector3.y, vector3.z);
         }
-        else
-        {
-            std::cerr << "Warning: Uniform '" << uniform_ID << "' not found in shader program " << program_uint << std::endl;
+        else {
+            std::cerr << "WARNING: Uniform '" << uniform_ID << "' not found in shader program " << ShaderProgram << std::endl;
         }
         #else
-        // In release builds, just set the uniform without checking
         glUniform3f(uniformCode, vector3.x, vector3.y, vector3.z);
         #endif
     }
-    static Shader getDefaultShader()
+
+    static inline const char* getDefaultVertexSource()
     {
-        static Shader DEFAULT_SHADER(DEFAULT_VTX_SHADER_SOURCE, DEFAULT_FRAG_SHADER_SOURCE);
-        return DEFAULT_SHADER;
+        return DEFAULT_VTX_SHADER_SOURCE;
     }
+    static inline const char* getDefaultFragmentSource()
+    {
+        return DEFAULT_FRAG_SHADER_SOURCE;
+    }
+
 
 private:
     static inline const char* DEFAULT_VTX_SHADER_SOURCE = R"(
     #version 330 core
-    layout(location = 0) in vec3 aPos;
-    uniform mat4 transform;
+    layout(location = 0) in vec2 aPos; // Position attribute
 
     void main() {
-        gl_Position = transform * vec4(aPos, 1.0);
+        // Set gl_Position directly from the vertex position
+        gl_Position = vec4(aPos, 0.0, 1.0); // 2D position with Z = 0.0 and W = 1.0
     }
+
     )";
 
     static inline const char* DEFAULT_FRAG_SHADER_SOURCE = R"(
-        #version 330 core
+    #version 330 core
 
-        in vec3 vertexColor; // Input from vertex Shader
-        out vec4 FragColor;
+    out vec4 FragColor; // Output color
 
-        uniform vec3 color; // Color uniform
+    void main()
+    {
+        FragColor = vec4(1.0, 1.0, 1.0, 1.0); // Set the color to white (RGBA)
+    }
 
-        void main() {
-            FragColor = vec4(color, 1.0); // Use the uniform color
-        }
+
     )";
-
 };
-
-
-
 
 #endif
